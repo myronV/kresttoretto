29776fec9b431d6cf313d940fe34d67cb9e165b5
Myron312 <traftar4ik@gmail.com>
2018-04-30 17:44:42 +0300

    finish

---
+++ krest-toreto/index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Крест Торетто со скидкой и быстрой доставкой по СНГ!</title>
+    <meta name="description" content="Крест Торетто со скидкой и быстрой доставкой по СНГ">
+    <meta name="keywords"
+          content="Крест Торетто, купить Крест Торетто, Крест Торетто цена ,Крест Торетто отзывы, Крест Торетто заказать">
+    <link rel="shortcut icon" href="https://bestshopby.ru/toretto4/favicon.ico" type="image/x-icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=480">
+    <link media="all" href="css/main.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link href="css/css.css" rel="stylesheet" type="text/css">
+
+    <script src="js/timer.js"></script>
+    <link href="css/css_002.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+
+<div class="main-wrap">
+    <div class="section block-1">
+        <div class="container-1 clearfix">
+            <div class="cost old-cost">
+                <p>Старая цена:</p>
+                <strong>1260 грн.</strong>
+            </div>
+            <div class="sale">
+                <p>Скидка</p>
+                <strong>53%</strong>
+            </div>
+            <div class="cost new-cost">
+                <p>Новая цена:</p>
+                <strong>590 грн.</strong>
+            </div>
+        </div>
+        <div class="container-2">
+            <div class="timer">
+                <p>До конца акции осталось:</p><br>
+                <script src="js/TimeCircles.js">
+                </script>
+                <div id="timer6cc94deb8168d75418be5ef86d3dcf63" style="min-width: 283px; height: 72px; text-align: center;">
+                </div>
+            </div>
+            <div class="attention">Осталось всего 9 шт. по акции!</div>
+            <div class="center"><a href="#delivery" class="button-m"></a></div>
+        </div>
+    </div>
+    <div class="section block-2" style="">
+
+        <div class="container">
+            <h2>Особенности Креста Доминика Торетто</h2>
+
+            <p style="padding-bottom:;">
+                <img alt="Крест Торетто" title="Крест Торетто" style="float:left; width:200px; margin-right:25px;"
+                     src="img/b1000000.jpg">
+                <span style="font-weight:bold; color:#E7AC00;">Качество</span><br>Материал, из которого изготавливается
+                крест, включает в себя специальные сплавы нескольких металлов. Камни - фианит.
+            </p>
+            <div style="clear:both;"></div>
+            <p></p>
+            <p style="padding-bottom:;">
+                <img alt="Крест Торетто" title="Крест Торетто" style="float:left; width:200px; margin-right:25px;"
+                     src="img/b2000000.jpg">
+                <span style="font-weight:bold; color:#E7AC00;">Характерный дизайн</span><br>Если
+                Вы внимательно наблюдали за героем «Форсажа» Домиником Торетто, то
+                могли заметить, что совершенно такой же крест был на шее актера Вин
+                Дизеля все 7 частей фильма.
+            </p>
+            <div style="clear:both;"></div>
+            <p></p>
+            <p style="padding-bottom:;">
+                <img alt="Крест Торетто" title="Крест Торетто" style="float:left; width:200px; margin-right:25px;"
+                     src="img/b3000000.jpg">
+                <span style="font-weight:bold; color:#E7AC00;">Исключительная особенность</span><br>Еще одной деталью,
+                позволяющей подчеркнуть незаурядность Вашего выбора, является нижняя подвижная часть креста.
+            </p>
+            <div style="clear:both;"></div>
+            <p></p>
+
+            <h2>Камни, которыми украшен Крест, имеют надежное крепление.</h2>
+            <center><img alt="Крест Торетто" title="Крест Торетто"
+                         style="width:auto; height:auto;padding-bottom:20px; float:none;"
+                         src="img/t4000000.jpg"></center>
+            <h2>ХАРАКТЕРИСТИКИ</h2>
+            <table>
+                <tbody>
+                <tr>
+                    <th>тип:</th>
+                    <td>реплика креста Вин Дизеля в «Форсаже»</td>
+                </tr>
+                <tr>
+                    <th>размер:</th>
+                    <td>5х4 см, вес 22 г</td>
+                </tr>
+                <tr>
+                    <th>длина цепочки:</th>
+                    <td>56 см</td>
+                </tr>
+                <tr>
+                    <th>материал:</th>
+                    <td>титан</td>
+                </tr>
+                <tr>
+                    <th>камни:</th>
+                    <td>природный цирконий</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <h2>Видео обзор</h2>
+            <div class="video-container">
+                <div class="youtube" id="pBFglk3PdFk"><img src="img/hqdefault.jpg" class="thumb">
+                    <div class="play"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- owmen17 -->
+    <div class="section block-7">
+        <h2>отзывы довольных клиентов</h2>
+        <div class="container">
+            <div class="rev rev-1">
+                <img alt="Крест Торетто" title="Крест Торетто" src="img/68400000.png">
+                <p class="rev-title">ВИКТОР</p>
+                <p class="rev-text">Спасибо Вам большое, крест
+                    забрал сегодня с почты и был удивлен его качеством и красотой, он точно
+                    как у Торетто. Берите не пожалеете! доставили за 7 дней.</p>
+            </div>
+            <div class="rev rev-2">
+                <img alt="Крест Торетто" title="Крест Торетто" src="img/av1.png">
+                <p class="rev-title">ИРИНА</p>
+                <p class="rev-text">Подарила брату, он фанат форсажа. Был в восторге, крест класнный, даже мне
+                    понравился! Спасибо за хорошее обслуживание!</p>
+            </div>
+            <div class="rev rev-3">
+                <img alt="Крест Торетто" title="Крест Торетто" src="img/ava00000.png">
+                <p class="rev-title">ДЕНИС</p>
+                <p class="rev-text">Спасибо большое этому
+                    интерент магазину, крест получил очень доволен, всем советую не
+                    пожелеете, и еще раз спасибо что дали такую возможность получить крест.
+                    для меня он очень дорог) Всем советую заказывать.</p>
+            </div>
+
+        </div>
+    </div>
+    <div class="section block-6">
+        <h2>Доставка и оплата</h2>
+        <div class="container">
+            <div class="deliv-item">
+                <span class="deliv-icon deliv-pic-1"></span>
+                <p>Доставка почтой в течение <br>от 1 до 10 рабочих дней.<br>Стоимость доставки товара от 0 грн.</p>
+            </div>
+            <div class="deliv-item">
+                <span class="deliv-icon deliv-pic-2"></span>
+                <p>Никаких предоплат! Оплата заказов осуществляется непосредственно по факту получения товара</p>
+            </div>
+            <div class="deliv-item">
+                <span class="deliv-icon deliv-pic-3"></span>
+                <p>Вы вправе отказаться от покупки в течение 14 дней с момента получения заказа, независимо от причины
+                    возврата.</p>
+            </div>
+        </div>
+    </div>
+    <div class="dot-line-2"></div>
+    <div class="section block-1">
+        <div class="container-1 clearfix">
+            <div class="cost old-cost">
+                <p>Старая цена:</p>
+                <strong>1260 грн.</strong>
+            </div>
+            <div class="sale">
+                <p>Скидка</p>
+                <strong>53%</strong>
+            </div>
+            <div class="cost new-cost">
+                <p>Новая цена:</p>
+                <strong>590 грн.</strong>
+            </div>
+        </div>
+        <div class="container-2" id="delivery">
+            <div class="order">
+                <p class="center">
+                    <strong>Оставьте заявку</strong><br>
+                    и мы свяжемся с вами для уточнения деталей заказа
+                </p>
+                <form action="zakaz.php" method="post"
+                      onsubmit="if(this.name.value==''){alert('Введите Ваше имя');return false}if(this.phone.value==''){alert('Введите Ваш номер телефона');return false}return true;">
+                    <div><input name="name" placeholder="Введите Ф.И.О" required="" type="text"></div>
+                    <div><input name="phone" placeholder="Введите телефон" required="" type="text"></div>
+                    <div>
+                        <button></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="section footer">
+        <div class="center">
+            <img alt="Крест Торетто" title="Крест Торетто" class="mob-vis" src="img/copy-b-320-405.jpg">
+            <p style="text-align: center">
+                <a style="color: inherit;" href="-page=politics.htm" target="_blank">Политика конфиденциальности</a>&nbsp;&nbsp;
+                <a style="color: inherit;" href="-page=agreement.htm" target="_blank">Пользовательское соглашение</a></p>
+            <p></p>
+        </div>
+    </div>
+</div>
+
+
+<link rel="stylesheet" type="text/css" href="css/roboto.css">
+<script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/plugins.js" type="text/javascript"></script>
+<script src="js/detect.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/previewYouTube.js"></script>
+
+<style type="text/css">.youtube {
+    background-color: #000;
+    max-width: 100%;
+    height: inherit;
+    overflow: hidden;
+    position: relative;
+    cursor: hand;
+    cursor: pointer
+}
+
+.youtube .thumb {
+    bottom: 0;
+    display: block;
+    left: 0;
+    margin: auto;
+    max-width: 100%;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100%;
+    height: auto
+}
+
+.youtube .play {
+    filter: alpha(opacity=80);
+    opacity: .8;
+    height: 77px;
+    left: 50%;
+    margin-left: -38px;
+    margin-top: -38px;
+    position: absolute;
+    top: 50%;
+    width: 77px;
+    background: url("//static.best-gooods.ru/img/youtube-play-icon.png") no-repeat
+}</style>
+<script src="js/slick.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" src="js/init.js"></script>
+</body>
+</html>
